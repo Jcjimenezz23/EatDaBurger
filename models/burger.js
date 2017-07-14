9ff208dd779596b468067a6208843f897d6baea6
Jc <jcjimenezz23@gmail.com>
2017-07-14 00:09:52 -0400

    made functions that will call the ORM functions

--- models/burger.js
+++ models/burger.js
@@ -1,4 +1,24 @@
 //importing orm.js file into burger.js
 var orm = require("../config/orm.js");
 
+//function that will call the ORM functions using specific input for the ORM
+var burgers = {
+	selectAll: function(cb) {
+		orm.selectAll("burgers", function(res) {
+			cb(res);
+		});
+	},
+	insertOne: function(cb) {
+		orm.insertOne("burgers", cols, vals, function(res) {
+			cb(res);
+		});
+	},
+	updateOne: function(cb) {
+		orm.updateOne("burgers", objColVals, condition, function(res) {
+			cb(res);
+		});
+	}
+};
 
+//exporting the database functions, basically running the file
+module.exports = burger;
